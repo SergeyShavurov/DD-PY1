8e88236c8c4e529797e0f19972b39f08a180347b
SergeyShavurov <115174346+SergeyShavurov@users.noreply.github.com>
2022-10-19 20:22:23 +0300

    Update main.py

--- LaboratoryWorks_3/task1/main.py
+++ LaboratoryWorks_3/task1/main.py
@@ -1,7 +1,7 @@
 src = not False and True or False and not True
 
-# TODO расписать упрощение выражения
-
-result = True
+result = True and True or False and False # Избавляемся от отрицания not
+result = True or False # Избавляемся от and
+result = True # избавляемся от or
 
 print(src == result)
