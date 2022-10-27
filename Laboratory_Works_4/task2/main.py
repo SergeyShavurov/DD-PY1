b21de410a28977cc223a29ddbe5662c9b94ee9dd
SergeyShavurov <115174346+SergeyShavurov@users.noreply.github.com>
2022-10-27 18:44:54 +0300

    Updated with edits from reviewer
    Внес правки согласно рекомендациям проверяющего.
    1) исправлена ошибка values - value
    2) добавлено value = round(value / sum_values * 100, 2)

--- Laboratory_Works_4/task2/main.py
+++ Laboratory_Works_4/task2/main.py
@@ -16,14 +16,14 @@ main_str = """
 print(get_count_char(main_str))
 
 def get_percent_char(dict_per): # пятый пункт задания Функция нахождения процента содержания каждого элемента
-    sum_values_ = sum(dict_per.values())  # сумма всех значений
-    dict_ = {}  # пустой словарь
+    sum_values = sum(dict_per.values())  # сумма всех значений
+    dict = {}  # пустой словарь
 
-    for key_, values_ in dict_per.items():  # вытаскиваем значения и ключи
-        values_ = values_ / sum_values_ * 100  # перезаписываем значение
-        dict_[key_] = values_  # записываем значение в словарь, в задании нет инфы касаемо округления, потому даны чистыe числа
+    for key, value in dict_per.items():  # вытаскиваем значения и ключи
+        value = round(value / sum_values * 100, 2) # перезаписываем значение округленные до 2 знака
+        dict[key] = value
 
-    return dict_
+    return dict
 
 print(get_percent_char(get_count_char(main_str)))
 
