1c0d44386f085a56f40f49bdb7d7de047f0fef9e
SergeyShavurov <115174346+SergeyShavurov@users.noreply.github.com>
2022-10-27 18:49:01 +0300

    Updated with edits from reviewer task3
    Внесены правки:
    1) вместо конструкции != None используется is not None
    2) в else вызывается сразу список без последнего элемента, без лишнего присваивания

--- Laboratory_Works_4/task3/main.py
+++ Laboratory_Works_4/task3/main.py
@@ -1,13 +1,11 @@
 def delete(list_, index=None):
-    if index != None:  # если индекс  задан то удаляем его
+    if index is not None:  # если индекс  задан то удаляем его
         new_list_ = list_[: index] # промежуточный лист
         list_ = new_list_ + list_[index + 1:]
         return list_
-
+        
     else:  # если индекс не задан то удаляем последний элемент
-        list_ = list_[:-1]
-        return list_
-        # Была мысль сделать через List_.pop(index) как лучше?
+        return list_[:-1]   
 
 print(delete([0, 0, 1, 2], index=0))  # [0, 1]
 print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
