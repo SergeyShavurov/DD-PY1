2fce9fff42b66de616a9aa536c76d2339c0ab518
SergeyShavurov <115174346+SergeyShavurov@users.noreply.github.com>
2022-11-10 21:07:23 +0300

    Update main.py

--- Laboratoty_Works_5/Task_3/main.py
+++ Laboratoty_Works_5/Task_3/main.py
@@ -5,7 +5,7 @@ def get_unique_list_numbers(list_length = 15) -> list[int]:
     while len(set_of_numbers) < list_length:
         set_of_numbers.add(randint(-10, 10))
 
-    unique_list_numbers= list(set_of_numbers)
+    unique_list_numbers = list(set_of_numbers)
     shuffle(unique_list_numbers) # если не использоват перемешивание, то значения упорядочиваются
 
     return unique_list_numbers
